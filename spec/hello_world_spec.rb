3aee6825a3a5e166d12373dc00c7fbdd94707172
Rahul Shah <rahulshah7@outlook.com>
2019-10-26 11:22:23 +0100

    Add return type test

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -3,6 +3,10 @@
 require 'hello_world'
 
 describe 'hello_world' do
+  it 'returns a string' do
+    expect(hello_world).to be_a(String)
+  end
+
   it 'returns the string "Hello, world!"' do
     expect(hello_world).to eq('Hello, world!')
   end
